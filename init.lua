bfee72697eaec60ab8bbb689cd8d9982a558f9ba
sameerdiwse <sameerdiwse9090@gmail.com>
2025-05-01 13:31:35 +0530

    Some changes in my lazy vim config

--- init.lua
+++ init.lua
@@ -33,6 +33,18 @@ require("lazy").setup({
     },
   },
   {
+  "nvimtools/none-ls.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.prettierd,
+      },
+    })
+  end,
+  },
+  {
     { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
   },
   {
@@ -70,4 +82,10 @@ require("nvim-treesitter.configs").setup({
 
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
+vim.api.nvim_create_autocmd("BufWritePre", {
+  callback = function()
+    vim.lsp.buf.format({ async = false })
+  end,
+})
+
 
--- lua/keymaps/keymaps.lua
+++ lua/keymaps/keymaps.lua
@@ -17,7 +17,23 @@ function M.setup()
   vim.keymap.set('n', '<leader>h', ':Neotree toggle_hidden<CR>', { noremap = true, silent = true })
 
   vim.keymap.set('n', '<leader>ff', ':Telescope find_files<CR>', { noremap = true, silent = true })
+
+
+  vim.keymap.set('n', '<leader>sv', ':vsplit<CR>', { noremap = true, silent = true })
+
+  vim.keymap.set('n', '<leader>sh', ':split<CR>', { noremap = true, silent = true })
+
+  vim.keymap.set('n', '<leader>wh', '<C-W>h', { noremap = true, silent = true })
   
+  vim.keymap.set('n', '<leader>wl', '<C-W>l', { noremap = true, silent = true })
+
+  vim.keymap.set('t', '<leader>tt', [[<C-\><C-n>:hide<CR>]], { noremap = true, silent = true })
+
+  vim.keymap.set('n', '<leader>q', ':q<CR>', { noremap = true, silent = true })  -- Close current window
+
+  vim.keymap.set('n', '<leader>f', 'f', { noremap = true })
+
+
 end
 
 return M
